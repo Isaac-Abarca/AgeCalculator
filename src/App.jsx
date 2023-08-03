df3f807992639481a07d1c00aa49b840c0fa4b70
Isaac Abarca Camacho <69941209+Isaac-Abarca@users.noreply.github.com>
2023-08-02 21:26:04 -0600

    Funcionalidad de calcular fecha.
    Funcionalidad terminada faltan detalles sobre limpiar campos y demas.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,101 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import{ useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [date, setDate] = useState("");
+  const [month, setMonth] = useState("");
+  const [year, setYear] = useState("");
+  const [age, setAge] = useState(null);
+
+  const calculateAge = () => {
+    const inputDate = parseInt(date);
+    const inputMonth = parseInt(month);
+    const inputYear = parseInt(year);
+    
+    // Validaciones
+    if (inputDate < 1 || inputDate > 31 || inputMonth < 1 || inputMonth > 12 || inputYear < 1900 || inputYear > 2500) {
+      alert("Por favor, ingresa valores válidos para la fecha, el mes y el año.");
+      return;
+    }
+
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth() + 1;
+    const currentDay = currentDate.getDate();
+
+    let years = currentYear - inputYear;
+    let months = currentMonth - inputMonth;
+    let days = currentDay - inputDate;
+
+    if (days < 0) {
+      months--;
+      days += new Date(inputYear, inputMonth - 1, 0).getDate();
+    }
+
+    if (months < 0) {
+      years--;
+      months += 12;
+    }
+
+    setAge({
+      years,
+      months,
+      days,
+    });
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <h1 className="title">Age Calculator</h1>
+        <div className="container">
+          <div className="container-date">
+            <h2>Date</h2>
+            <input
+              className="input"
+              type="number"
+              name="Date"
+              id="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </div>
+          <div className="container-month">
+            <h2>Month</h2>
+            <input
+              className="input"
+              type="number"
+              name="Month"
+              id="month"
+              value={month}
+              onChange={(e) => setMonth(e.target.value)}
+            />
+          </div>
+          <div className="container-year">
+            <h2>Year</h2>
+            <input
+              className="input"
+              type="number"
+              name="Year"
+              id="year"
+              value={year}
+              onChange={(e) => setYear(e.target.value)}
+            />
+          </div>
+        </div>
+        <button className="btn-calculate" onClick={calculateAge}>
+          Calcular
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+        {age && (
+        <p className="read-the-docs">
+          Tu edad es: {age.years} años, {age.months} meses y {age.days} días.
         </p>
+      )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
